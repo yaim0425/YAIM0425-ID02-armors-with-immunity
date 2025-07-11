5abaf383d938c6e3b973810a722c5ab2b4e8e431
yaim0425 <yaim0425@gmail.com>
2025-07-11 07:28:37 -0500

    Modificación del archivo settings

--- settings.lua
+++ settings.lua
@@ -1,17 +1,63 @@
-data:extend({
-	{
-		type = "string-setting",
-		name = "zzzYAIM0425-0200-armor-base",
-		order = "1",
-		setting_type = "startup",
-		default_value = "light-armor",
-        allowed_values = {
-            "light-armor",
-            "heavy-armor",
-            "modular-armor",
-            "power-armor",
-            "power-armor-mk2",
-            "mech-armor"
-        }
-	}
-})
+---------------------------------------------------------------------------------------------------
+---> settings-final-fixes.lua <---
+---------------------------------------------------------------------------------------------------
+
+--- Cargar las funciones
+require("__zzzYAIM0425-0000-lib__.settings-final-fixes")
+
+---------------------------------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------------------------------
+
+--- Configuración inicial
+local settings = {
+    type = "string-setting",
+    name = GPrefix.name .. "-0200-armor-base",
+    order = "1",
+    setting_type = "startup",
+    default_value = "light-armor",
+    allowed_values = {
+        "light-armor",
+        "heavy-armor",
+        "modular-armor",
+        "power-armor",
+        "power-armor-mk2",
+    }
+}
+
+-- Krastorio
+if mods["Krastorio2"] then
+    table.insert(settings.allowed_values, "kr-power-armor-mk3")
+    table.insert(settings.allowed_values, "kr-power-armor-mk4")
+end
+
+--- Youki Industries
+if mods["Yuoki"] then
+    table.insert(settings.allowed_values, "yi_armor_gray")
+    table.insert(settings.allowed_values, "yi_armor_red")
+    table.insert(settings.allowed_values, "yi_armor_gold")
+    table.insert(settings.allowed_values, "yi_walker_a")
+    table.insert(settings.allowed_values, "yi_walker_c")
+end
+
+--- Space Ag
+if mods["space-age"] then
+    table.insert(settings.allowed_values, "mech-armor")
+end
+
+---------------------------------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------------------------------
+
+--- Cargar la configuración
+data:extend({ settings })
+
+---------------------------------------------------------------------------------------------------
